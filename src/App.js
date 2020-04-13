05d02789c8d4897832ff350a2f3673ede11a4287
bambooka <onceup0nAtime>
2020-04-13 12:34:51 +0300

    feat: add default header and footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/common/Header'
+import Footer from './components/common/Footer'
+import Maps from './components/common/Map'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Maps />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/common/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class Footer extends React.Component {
+  render(){
+    return (
+      <footer>
+        COPYRIGHT (C) 2020 RESTAURANT REVIEWS ALL RIGHTS RESERVED.
+      </footer>
+    )
+  }
+}
