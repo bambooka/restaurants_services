bb767918966c7a1e900c69b7396f305fde7c182a
bambooka <onceup0nAtime>
2020-05-01 11:14:41 +0300

    feat: install and put to cache

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -139,3 +139,56 @@ export function unregister() {
       });
   }
 }
+const CACHE = 'cache-v1';
+
+var toCache = [
+  'index.html',
+  'restaurant.html?id=1',
+  'restaurant.html?id=2',
+  'restaurant.html?id=3',
+  'restaurant.html?id=4',
+  'restaurant.html?id=5',
+  'restaurant.html?id=6',
+  'restaurant.html?id=7',
+  'restaurant.html?id=8',
+  'restaurant.html?id=9',
+  'restaurant.html?id=10',
+  './dist/style.min.css',
+  './js/main.js',
+  './js/restaurant_info.js',
+  './js/dbhelper.js',
+  './img/placeholder.jpg',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg',
+  './img/smallplaceholder.jpg',
+  './img/small1.jpg',
+  './img/small2.jpg',
+  './img/small3.jpg',
+  './img/small4.jpg',
+  './img/small5.jpg',
+  './img/small6.jpg',
+  './img/small7.jpg',
+  './img/small8.jpg',
+  './img/small9.jpg',
+  './img/small10.jpg'
+];
+
+// Install and put into cache
+window.self.addEventListener('install', function (event) {
+
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(function (cache) {
+        return cache.addAll(toCache);
+      })
+  );
+});
+
